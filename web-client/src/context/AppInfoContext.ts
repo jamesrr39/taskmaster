fc9e8a37adfb399971ce3b8981edf8e5f6c69cb2
James <jr39.dev@gmail.com>
2026-08-13 23:15:22 +0200

    remove unused files

--- web-client/src/context/AppInfoContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "preact";
-import { HttphandlersInfoResponse } from "../openapi/generated/tracksAppSchemas";
-
-export const AppInfoContext = createContext<HttphandlersInfoResponse|undefined>(undefined);
-
